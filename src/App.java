b49e1c0a3a60cca0c00c58d5cbb75cbbd99971a5
JomaGsm <jomagsm@gmail.com>
2023-01-13 20:42:24 +0600

    finish hm4

--- src/App.java
+++ src/App.java
@@ -1,40 +1,49 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class App {
         public static void main(String[] args) throws Exception {
-                final String separator = "==========+++==========";
-                ArrayList<String> colors = new ArrayList<>();
-                ArrayList<Player> players = new ArrayList<>();
+                ArrayList<String> listA = new ArrayList<String>();
+                ArrayList<String> listB = new ArrayList<String>();
+                ArrayList<String> listC = new ArrayList<String>();
+                listA = getListOfNames();
+                printSeparator(listA);
+                listB = getListOfNames();
+                printSeparator(listB);
+                listC = mergeLists(listA, listB);
+                printSeparator(listC);
+                Collections.sort(listC, Comparator.comparing(String::length));
+                printSeparator(listC);
+                ;
+        }
 
-                colors.add("Red");
-                colors.add("Green");
-                colors.add("Black");
-                colors.add("White");
-                colors.add("Pink");
-                colors.add("Yellow");
-                System.out.println(colors);
-                System.out.println(separator);
-                Collections.sort(colors);
-                System.out.println(colors);
-                System.out.println(separator);
-                Collections.reverse(colors);
-                System.out.println(colors);
-                System.out.println(separator);
-                Collections.shuffle(colors);
-                System.out.println(colors);
-                System.out.println(separator);
-                for (String item : colors) {
-                        System.out.println(item);
+        public static ArrayList<String> getListOfNames() {
+                ArrayList<String> temp = new ArrayList<String>();
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Input name: ");
+                for (int i = 0; i < 5; i++) {
+                        temp.add(scanner.next());
                 }
+                return temp;
+        }
 
-                Player player1 = new Player("Mack", 1);
-                Player player2 = new Player("Jack", 7);
-                Player player3 = new Player("Macle", 9);
+        public static ArrayList<String> mergeLists(ArrayList<String> first, ArrayList<String> second) {
+                ArrayList<String> temp = new ArrayList<String>();
+                Collections.reverse(second);
+                if (first.size() == second.size()) {
+                        for (int i = 0; i < first.size(); i++) {
+                                temp.add(first.get(i));
+                                temp.add(second.get(i));
+                        }
+                }
+                return temp;
+        }
 
-                players.add(player1);
-                players.add(player2);
-                players.add(player3);
-                System.out.println(players);
+        public static void printSeparator(ArrayList<String> first) {
+                final String separator = "==========+++==========";
+                System.out.println(first);
+                System.out.println(separator);
         }
 }
